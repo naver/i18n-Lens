480e4df665452b40ce5e998922fa9f59879176ad
njh7799 <rainy_waltz@naver.com>
2023-01-10 18:03:58 +0900

    [Feature] Create util functions with test codes
    - `flattenObject`
      - flatten the input object to 1-layer object.
    - `getDistanceBetweenDirectories`
      - returns the distance between two different directories.

---
+++ src/utils.ts
@@ -0,0 +1,29 @@
+export function flattenObject(parentObject: object) {
+  let object: object = parentObject;
+  while (Object.values(object).some((value) => typeof value === 'object')) {
+    object = Object.entries(object).reduce((obj: any, [key, subObject]) => {
+      if (typeof subObject === 'object') {
+        return { ...obj, ...subObject };
+      }
+      return { ...obj, [key]: subObject };
+    }, {});
+  }
+  return object;
+}
+
+export function getDistanceBetweenDirectories(
+  uriPathLeft: string,
+  uriPathRight: string,
+) {
+  const routeStackLeft = uriPathLeft.split('/').filter((route) => route);
+  const routeStackRight = uriPathRight.split('/').filter((route) => route);
+  while (
+    routeStackLeft[0] &&
+    routeStackRight[0] &&
+    routeStackLeft[0] === routeStackRight[0]
+  ) {
+    routeStackLeft.shift();
+    routeStackRight.shift();
+  }
+  return routeStackLeft.length + routeStackRight.length;
+}
